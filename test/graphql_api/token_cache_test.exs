52b7986a2057c668faadc20e567a763e4458a595
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-09-20 11:55:11 -0700

    add more cache tests

--- test/graphql_api/token_cache_test.exs
+++ test/graphql_api/token_cache_test.exs
@@ -3,9 +3,15 @@ defmodule GraphqlApi.TokenCacheTest do
 
   alias GraphqlApi.TokenCache
 
-  describe "get/1" do
-    test "returns nil when given key is not found" do
+  describe "get/1 and put/2" do
+    test "get/1 returns nil of key is not found, put/2 stores or updates a given key value pair" do
       assert nil === TokenCache.get(0)
+      timestamp = DateTime.utc_now()
+      assert true === TokenCache.put(0, %{token: "FakeToken", timestamp: timestamp})
+      assert %{token: "FakeToken", timestamp: timestamp} === TokenCache.get(0)
+      new_timestamp = DateTime.utc_now()
+      assert true === TokenCache.put(0, %{token: "FakeToken", timestamp: new_timestamp})
+      assert %{token: "FakeToken", timestamp: new_timestamp} === TokenCache.get(0)
     end
   end
 end
