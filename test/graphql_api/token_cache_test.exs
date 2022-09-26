3feed6eca651e76ab5916d2ae1b969e41b4c5cd1
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-09-26 15:39:02 -0700

    change formatting ab it

--- test/graphql_api/token_cache_test.exs
+++ test/graphql_api/token_cache_test.exs
@@ -8,7 +8,8 @@ defmodule GraphqlApi.TokenCacheTest do
   setup :user
 
   describe "get/1 and put/2" do
-    test "get/1 returns nil of key is not found, put/2 stores or updates a given key value pair", %{user: %{id: id}} do
+    test "get/1 returns nil of key is not found, put/2 stores or updates a given key value pair",
+         %{user: %{id: id}} do
       assert nil === TokenCache.get(id)
       timestamp = DateTime.utc_now()
       assert :ok === TokenCache.put(id, %{token: "FakeToken", timestamp: timestamp})
--- test/graphql_api_web/schema/subscriptions/auth_token_test.exs
+++ test/graphql_api_web/schema/subscriptions/auth_token_test.exs
@@ -19,7 +19,10 @@ defmodule GraphqlApiWeb.Schema.Subscriptions.AuthTokenTest do
   setup :user
 
   describe "@auth_token_generated" do
-    test "broadcasts when an auth_token for the given ID is generated", %{socket: socket, user: %{id: id}} do
+    test "broadcasts when an auth_token for the given ID is generated", %{
+      socket: socket,
+      user: %{id: id}
+    } do
       string_id = to_string(id)
       ref = push_doc(socket, @auth_token_generated_doc, variables: %{"user_id" => string_id})
 
@@ -28,20 +31,19 @@ defmodule GraphqlApiWeb.Schema.Subscriptions.AuthTokenTest do
       assert_push("subscription:data", data)
 
       assert %{
-        subscriptionId: ^subscription_id,
-        result: %{
-          data: %{
-            "authTokenGenerated" => %{
-              "timestamp" => timestamp,
-              "token" => @token,
-              "user_id" => ^string_id
-            }
-          }
-        }
-      } = data
+               subscriptionId: ^subscription_id,
+               result: %{
+                 data: %{
+                   "authTokenGenerated" => %{
+                     "timestamp" => timestamp,
+                     "token" => @token,
+                     "user_id" => ^string_id
+                   }
+                 }
+               }
+             } = data
 
       assert {:ok, @timestamp, 0} === DateTime.from_iso8601(timestamp)
-
     end
   end
 end
