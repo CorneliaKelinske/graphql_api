2865b8071b9bd74e8469efe078c1add4517d323e
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-09-26 15:35:53 -0700

    add auth_token_subscription test

--- test/graphql_api_web/schema/subscriptions/auth_token_test.exs
+++ test/graphql_api_web/schema/subscriptions/auth_token_test.exs
@@ -1,5 +1,7 @@
 defmodule GraphqlApiWeb.Schema.Subscriptions.AuthTokenTest do
   use GraphqlApiWeb.SubscriptionCase
+  import GraphqlApi.UserFixtures, only: [user: 1]
+  alias GraphqlApi.TokenCache
 
   @auth_token_generated_doc """
   subscription AuthTokenGenerated($user_id: ID!) {
@@ -11,11 +13,35 @@ defmodule GraphqlApiWeb.Schema.Subscriptions.AuthTokenTest do
   }
   """
 
+  @token "FakeToken"
+  @timestamp DateTime.utc_now()
+
+  setup :user
+
   describe "@auth_token_generated" do
-    test "broadcasts when an auth_token for the given ID is generated", %{socket: socket} do
-      ref = push_doc(socket, @auth_token_generated_doc, variables: %{"user_id" => "2"})
+    test "broadcasts when an auth_token for the given ID is generated", %{socket: socket, user: %{id: id}} do
+      string_id = to_string(id)
+      ref = push_doc(socket, @auth_token_generated_doc, variables: %{"user_id" => string_id})
 
       assert_reply ref, :ok, %{subscriptionId: subscription_id}
+      TokenCache.put(id, %{token: @token, timestamp: @timestamp})
+      assert_push("subscription:data", data)
+
+      assert %{
+        subscriptionId: ^subscription_id,
+        result: %{
+          data: %{
+            "authTokenGenerated" => %{
+              "timestamp" => timestamp,
+              "token" => @token,
+              "user_id" => ^string_id
+            }
+          }
+        }
+      } = data
+
+      assert {:ok, @timestamp, 0} === DateTime.from_iso8601(timestamp)
+
     end
   end
 end
